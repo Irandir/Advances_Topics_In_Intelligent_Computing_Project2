5812cfe4dad9b5638f6ce3eaf7480b20988e9715
Irandir <Irandir@DESKTOP-TFS3Q09>
2018-09-06 13:27:14 -0300

    pso teste

---
+++ src/pso/PSO.java
@@ -0,0 +1,217 @@
+package pso;
+
+public class PSO {
+	double[][] base;
+
+	double[][] input; /* base */
+	double[] output; /* saida */
+	
+	double[][] particle;		 /* População */
+	double[][] velocity;   		/* Velocidade de ajuste */
+	double[] particleFitness;      /* Erro quadratico medio*/
+	double[][] pBest;      		/* Memoria anterior */
+	double[] pBestFitness;
+	double[] gBest;        		/* melhor particula*/
+	double[] gBestFitness;
+	double c1,c2;
+	double min,max;
+	
+	double wInertia;
+	double maxInertia;
+	double minInertia;
+	
+
+	public PSO(double[][] input, double[] output,int populationSize, double c1, double c2, 
+			 double wInertia, double maxInertia,double minInertia,double min,double max) {
+		super();
+		
+		this.input = input;
+		this.output = output;
+		this.c1 = c1;
+		this.c2 = c2;
+		this.wInertia = wInertia;
+		this.maxInertia = maxInertia;
+		this.minInertia = minInertia;
+		this.min = min;
+		this.max = max;
+
+		//depepende do tipo de modelo exp,linear e extc
+		int weights = input[0].length;//linear
+		
+		this.particle = new double[populationSize][weights];
+		this.velocity = new double[populationSize][weights];
+		this.pBest = new double[populationSize][ weights];
+		this.gBest = new double[weights];
+		this.particleFitness = new double[populationSize];
+		this.pBestFitness = new double[populationSize];
+		
+		this.generatePopulation();
+	}
+	
+	public void start (int epooc) {
+		this.gBestFitness = new double[epooc];
+		for (int i = 0; i < epooc; i++) {
+			this.calc_fitness();
+			this.calc_gBest(i);
+			this.populationAjust();
+			this.inertiaAjust(i, epooc);
+		}
+	}
+
+	/* Gerar pupulação, velocidade de ajuste e memoria inicial */	
+	public void  generatePopulation () {
+		
+		for (int i = 0; i < this.particle.length; i++) {
+			for (int j = 0; j < this.particle[0].length; j++) {
+				this.particle[i][j] = Math.random();
+				this.pBest[i][j] = this.particle[i][j];
+				this.velocity[i][j] = this.particle[i][j];
+			}
+		}
+	}
+	
+	/* calculando melhor particula a partir do erro quadratico medio */
+	public void calc_fitness() {
+		
+		double outputO = 0;
+		
+		double error;
+		double errorTotal = 0;
+
+		for (int k = 0; k < this.particle.length; k++) {
+			
+			outputO = 0;
+			errorTotal = 0;
+			for (int i = 0; i < this.input.length; i++) {
+				outputO = 0;
+				for (int j = 0; j < this.input[0].length; j++) {
+
+					outputO += this.particle[k][j] * this.input[i][j];//saida linear
+
+				}
+
+				error = (this.output[i] - outputO);
+
+				errorTotal += Math.pow(error, 2); /* eé aqui meu filhoooooo */
+
+				error = 0;
+
+			}
+			
+			errorTotal = errorTotal / this.input.length;
+			
+			/* criando / modificando memoria anterior das particulas */
+			
+			if(this.particleFitness[k] > 0) {
+				if(errorTotal < this.particleFitness[k]) {
+					for (int i = 0; i < this.particle[0].length; i++) {
+						this.pBest[k][i] = this.particle[k][i];
+					}
+					this.particleFitness[k] = errorTotal;
+					this.pBestFitness[k] = errorTotal;
+				}else {
+					this.particleFitness[k] = errorTotal;
+				}
+			}else {
+				this.particleFitness[k] = errorTotal;
+				this.pBestFitness[k] = errorTotal;
+			}
+			
+			errorTotal = 0;
+		}
+	}
+
+	public void calc_gBest (int index) {
+	/* encontrando melhor particula da população */
+		
+		int i = this.minFitness(pBestFitness);
+		
+		if(index != 0) {
+
+			if(gBestFitness[index-1] > this.pBestFitness[i]) {
+
+				gBestFitness[index] = pBestFitness[i];
+				
+				for (int j = 0; j < this.gBest.length; j++) {
+					this.gBest[j] = this.pBest[i][j];
+				}
+					
+			}else {
+				gBestFitness[index] = gBestFitness[index-1];
+			}
+		}else {
+			
+			gBestFitness[index] = pBestFitness[i];
+			
+			for (int j = 0; j < this.gBest.length; j++) {
+				this.gBest[j] = this.pBest[i][j];
+			}
+		}
+		
+		
+	}
+	
+	/* Metodo de ajuste da população */
+	public void populationAjust() {
+		
+		this.velocityAjust();
+		
+		for (int i = 0; i < particle.length; i++) {
+			for (int j = 0; j < particle[0].length; j++) {
+				this.particle[i][j] = this.particle[i][j] + this.velocity[i][j];
+			}
+		}
+	}
+	/* metodo de calculo da velocidade */
+	public void velocityAjust () {
+		for (int i = 0; i < this.velocity.length; i++) {
+			for (int j = 0; j < this.velocity[0].length; j++) {
+				//System.out.println("Antes =>"+this.velocity[i][j]);
+				this.velocity[i][j] = (this.wInertia * this.velocity[i][j]) + this.c1 * 
+						Math.random() * (this.pBest[i][j] - this.particle[i][j]) + this.c2 * 
+						Math.random() * (this.gBest[j] - this.particle[i][j]);
+				
+				//System.out.println("Depois =>"+this.velocity[i][j]);
+			}
+		}
+	}
+	
+	
+	public int minFitness (double[] value) {
+		
+		double min = 999999;
+		int index = 0;
+		
+		for (int i = 0; i < value.length; i++) {
+			
+			if(min > value[i] ) {
+				min = value[i];
+                index = i;
+			}
+		}
+		
+		return index;
+	}
+		
+	
+	public double  denormalize (double value) {
+		return value * (this.max - this.min) + this.max;
+	}
+	
+	
+	public void inertiaAjust(int index, int epooc) {
+		this.wInertia = (this.maxInertia - index / epooc) * (this.maxInertia - this.minInertia);
+	}
+	
+	public double[] getgBestFitness() {
+		return gBestFitness;
+	}
+
+	public double[] getgBest() {
+		return gBest;
+	}
+
+
+
+
+}
---
+++ src/pso/Teste.java
@@ -0,0 +1,21 @@
+package pso;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		double input[][] = { { 1, 2 }, { 2, 3 }, { 3, 4 }, { 4, 5 } };
+		double output[] = { 50.0, 80.0, 110.0, 140.0 };
+		PSO pso = new PSO(input, output, 10, 2, 2, 1, 6, 2, 0,0 );
+		pso.start(100);
+		double[] r = pso.getgBest();
+		for (int i = 0; i < r.length; i++) {
+			System.out.println(r[i]);
+		}
+		System.out.println("___________________________________");
+		for (int i = 0; i < output.length; i++) {
+			double s = (input[i][0]*r[0]+input[i][1]*r[1]);
+			System.out.println(s);
+		}
+	}
+
+}
